fc75c7ca4207bf9de0f420aee66b9931622d0d27
diegomes <R@fael2906>
2023-04-19 16:25:45 -0300

    adicionando comentários

--- src/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/PrimeiroPrograma.java
@@ -1,5 +1,11 @@
 package fundamentos;
 
+/**
+ *
+ * @author diegomes
+ * @since 19/04/2023
+ *
+ */
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         System.out.println("Primeiro Programa parte #01");
