73fa783f02fc41f4ed7d802828522c2f9093c576
diegomes <R@fael2906>
2023-04-19 16:12:24 -0300

    teste de alteração no git

--- src/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/PrimeiroPrograma.java
@@ -3,7 +3,6 @@ package fundamentos;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         System.out.println("Primeiro Programa parte #01");
-        System.out.println("Primeiro Programa parte #02");
 
     }
 }
