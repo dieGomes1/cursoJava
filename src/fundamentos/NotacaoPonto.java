4c2a224bc5eb2d4a9795d89a551e1460e6045f2f
diegomes <R@fael2906>
2023-04-25 15:59:04 -0300

    add arquivo sobre notação ponto

---
+++ src/fundamentos/NotacaoPonto.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class NotacaoPonto {
+    public static void main(String[] args) {
+
+        String b = "Bom dia!";
+
+        b = b.replace("!", "! Diego");
+
+        System.out.println(b.toUpperCase());
+
+
+    }
+}
