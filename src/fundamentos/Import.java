f85e73e005d1a023e90dda3c37b210a47a6063d3
diegomes <R@fael2906>
2023-04-25 16:08:03 -0300

    add arquivo sobre import

---
+++ src/fundamentos/Import.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+
+import java.util.Date;
+
+public class Import {
+    public static void main(String[] args) {
+
+        String dia = "Bom dia !!!";
+        System.out.println(dia);
+
+        Date mes = new Date();
+        System.out.println(mes);
+    }
+}
